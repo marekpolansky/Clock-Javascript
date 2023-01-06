500465e83fa3f5a8d4853a4f4654cdd50ff10a02
Marek Polasnky <marek.polansky@student.tuke.sk>
2023-01-06 17:06:23 +0100

    last commit

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ let hours = date.getHours();
 let minutes = date.getMinutes();
 setInterval(sec,1000);
 let skuska;
+let pomocnas = '0';
+let pomocnam = '0';
+let pomocnah = '0';
 function sec(){
+    
     let date = new Date();
-    skuska = date.getSeconds();
-    console.log(skuska);
+    s = date.getSeconds();
+    if (date.getHours() > 9){
+        pomocnah = '';
+    }
+    if (date.getMinutes() > 9){
+        pomocnam = '';
+    }
+    if (date.getSeconds() > 9){
+        pomocnas = '';
+    }
+    if (date.getHours() == 0){
+        pomocnah = '0';
+    }
+    if (date.getMinutes() == 0){
+        pomocnam = '0';
+    }
+    if (date.getSeconds() == 0){
+        pomocnas = '0';
+    }
+    clockRef.innerText = pomocnah + date.getHours() + " : " + pomocnam + date.getMinutes() + " : " + pomocnas + date.getSeconds();
 }
 
 
 
-//clockRef.innerText = '';
+//clockRef.innerText = 'Pica';
